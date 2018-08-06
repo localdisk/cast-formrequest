be2c02a7a5234244755122a9dde04ca6383594bf
localdisk <smoochynet@gmail.com>
2018-08-06 14:15:51 +0900

    initial commit.

---
+++ src/CastAttribute.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace Localdisk\Request;
+
+use DateTimeInterface;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+
+trait CastAttribute
+{
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation(): void
+    {
+        foreach ($this->getCasts() as $key => $value) {
+            if (! $this->has($key)) {
+                continue;
+            }
+
+            $this->merge([$key => $this->castAttribute($key, $this->input($key))]);
+        }
+    }
+
+    /**
+     * Cast an attribute to a native PHP type.
+     *
+     * @param  string $key
+     * @param  mixed $value
+     *
+     * @return mixed
+     */
+    protected function castAttribute($key, $value)
+    {
+        if ($value === null) {
+            return $value;
+        }
+
+        switch ($this->getCastType($key)) {
+            case 'int':
+            case 'integer':
+                return (int) $value;
+            case 'real':
+            case 'float':
+            case 'double':
+                return (float) $value;
+            case 'string':
+                return (string) $value;
+            case 'bool':
+            case 'boolean':
+                return (bool) $value;
+            case 'array':
+                if (is_array($value)) {
+                    return $value;
+                }
+
+                return array($value);
+            case 'json':
+                return $this->fromJson($value);
+            case 'collection':
+                return new Collection($value);
+            case 'date':
+                return $this->asDate($value);
+            case 'datetime':
+            case 'custom_datetime':
+                return $this->asDateTime($value);
+            case 'timestamp':
+                return $this->asTimestamp($value);
+            default:
+                return $value;
+        }
+    }
+
+    /**
+     * Get the type of cast for a model attribute.
+     *
+     * @param  string $key
+     *
+     * @return string
+     */
+    protected function getCastType($key): string
+    {
+        if ($this->isCustomDateTimeCast($this->getCasts()[$key])) {
+            return 'custom_datetime';
+        }
+
+        return strtolower(trim($this->getCasts()[$key]));
+    }
+
+    /**
+     * @return array
+     */
+    protected function getCasts(): array
+    {
+        if (property_exists($this, 'casts')) {
+            return $this->casts;
+        }
+
+        return [];
+    }
+
+    /**
+     * Decode the given JSON back into an array or object.
+     *
+     * @param  string $value
+     * @param  bool $asObject
+     *
+     * @return mixed
+     */
+    public function fromJson($value, $asObject = false)
+    {
+        return json_decode($value, ! $asObject);
+    }
+
+    /**
+     * Return a timestamp as DateTime object with time set to 00:00:00.
+     *
+     * @param  mixed $value
+     *
+     * @return \Illuminate\Support\Carbon
+     */
+    protected function asDate($value): Carbon
+    {
+        return $this->asDateTime($value)->startOfDay();
+    }
+
+    /**
+     * Return a timestamp as DateTime object.
+     *
+     * @param  mixed $value
+     *
+     * @return \Illuminate\Support\Carbon
+     */
+    protected function asDateTime($value): Carbon
+    {
+        // If this value is already a Carbon instance, we shall just return it as is.
+        // This prevents us having to re-instantiate a Carbon instance when we know
+        // it already is one, which wouldn't be fulfilled by the DateTime check.
+        if ($value instanceof Carbon) {
+            return $value;
+        }
+
+        // If the value is already a DateTime instance, we will just skip the rest of
+        // these checks since they will be a waste of time, and hinder performance
+        // when checking the field. We will just return the DateTime right away.
+        if ($value instanceof DateTimeInterface) {
+            return new Carbon(
+                $value->format('Y-m-d H:i:s.u'), $value->getTimezone()
+            );
+        }
+
+        // If this value is an integer, we will assume it is a UNIX timestamp's value
+        // and format a Carbon object from this timestamp. This allows flexibility
+        // when defining your date fields as they might be UNIX timestamps here.
+        if (is_numeric($value)) {
+            return Carbon::createFromTimestamp($value);
+        }
+
+        // If the value is in simply year, month, day format, we will instantiate the
+        // Carbon instances from that format. Again, this provides for simple date
+        // fields on the database, while still supporting Carbonized conversion.
+        if ($this->isStandardDateFormat($value)) {
+            return Carbon::createFromFormat('Y-m-d', $value)->startOfDay();
+        }
+
+        // Finally, we will just assume this date is in the format used by default on
+        // the database connection and use that format to create the Carbon object
+        // that is returned back out to the developers after we convert it here.
+        return Carbon::createFromFormat(
+            str_replace('.v', '.u', $this->getDateFormat()), $value
+        );
+    }
+
+    /**
+     * Determine if the given value is a standard date format.
+     *
+     * @param  string  $value
+     * @return bool
+     */
+    protected function isStandardDateFormat($value): bool
+    {
+        return preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2})$/', $value);
+    }
+
+    /**
+     * Get the format for database stored dates.
+     *
+     * @return string
+     */
+    public function getDateFormat(): string
+    {
+        return $this->dateFormat ?: 'U';
+    }
+
+    /**
+     * Return a timestamp as unix timestamp.
+     *
+     * @param  mixed $value
+     *
+     * @return int
+     */
+    protected function asTimestamp($value): int
+    {
+        return $this->asDateTime($value)->getTimestamp();
+    }
+
+    /**
+     * Determine if the cast type is a custom date time cast.
+     *
+     * @param  string $cast
+     *
+     * @return bool
+     */
+    protected function isCustomDateTimeCast($cast): bool
+    {
+        return strncmp($cast, 'date:', 5) === 0 ||
+            strncmp($cast, 'datetime:', 9) === 0;
+    }
+
+}
---
+++ tests/CastRequestTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Localdisk\Tests;
+
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Translation\Translator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\Factory;
+use Localdisk\Request\CastAttribute;
+use Mockery;
+use PHPUnit\Framework\Constraint\IsType;
+use PHPUnit\Framework\TestCase;
+
+class CastRequestTest extends TestCase
+{
+    public function testCast(): void
+    {
+        $params = [
+            'intAttribute' => '1',
+            'floatAttribute' => '4.0',
+            'stringAttribute' => 2,
+            'boolAttribute' => '1',
+            'arrayAttribute' => [
+                1,
+                2
+            ],
+            'scalarAttribute' => 'hoge',
+            'jsonAttribute' => '{"a":1,"b":2,"c":3,"d":4,"e":5}',
+            'collectionAttribute' => [
+                1,
+                2
+            ],
+            'dateAttribute' => '1969-07-20',
+            'datetimeAttribute' => '1969-07-20 22:56:00',
+            'timestampAttribute' => '1969-07-20 22:56:00'
+        ];
+        $query = http_build_query($params);
+        $request = $this->createRequest('?' . $query);
+        $request->validateResolved();
+
+        $this->assertInternalType(IsType::TYPE_INT, $request->input('intAttribute'));
+        $this->assertInternalType(IsType::TYPE_FLOAT, $request->input('floatAttribute'));
+        $this->assertInternalType(IsType::TYPE_STRING, $request->input('stringAttribute'));
+        $this->assertInternalType(IsType::TYPE_BOOL, $request->input('boolAttribute'));
+        $this->assertInternalType(IsType::TYPE_ARRAY, $request->input('arrayAttribute'));
+        $this->assertEquals([1, 2], $request->input('arrayAttribute'));
+        $this->assertInternalType(IsType::TYPE_ARRAY, $request->input('scalarAttribute'));
+        $this->assertEquals(['hoge'], $request->input('scalarAttribute'));
+        $this->assertInternalType(IsType::TYPE_ARRAY, $request->input('jsonAttribute'));
+        $this->assertInstanceOf(Collection::class, $request->input('collectionAttribute'));
+        $this->assertInstanceOf(\Illuminate\Support\Carbon::class, $request->input('dateAttribute'));
+        $this->assertEquals('1969-07-20', $request->input('dateAttribute')->toDateString());
+        $this->assertInstanceOf(\Illuminate\Support\Carbon::class, $request->input('datetimeAttribute'));
+        $this->assertEquals('1969-07-20 22:56:00', $request->input('datetimeAttribute')->toDateTimeString());
+        $this->assertEquals(-14173440, $request->input('timestampAttribute'));
+
+    }
+
+    /**
+     * Create a new request of the given type.
+     *
+     * @param string $url
+     *
+     * @return \Illuminate\Foundation\Http\FormRequest
+     */
+    protected function createRequest(string $url): FormRequest
+    {
+        $container = tap(new Container, function(Container $container) {
+            $container->instance(
+                \Illuminate\Contracts\Validation\Factory::class,
+                $this->createValidationFactory($container)
+            );
+        });
+
+        $request = CastFormRequest::create($url);
+
+        return $request->setContainer($container);
+    }
+
+    /**
+     * Create a new validation factory.
+     *
+     * @param  \Illuminate\Container\Container $container
+     *
+     * @return \Illuminate\Validation\Factory
+     */
+    protected function createValidationFactory($container): Factory
+    {
+        $translator = Mockery::mock(Translator::class)->shouldReceive('trans')
+            ->zeroOrMoreTimes()->andReturn('error')->getMock();
+
+        return new Factory($translator, $container);
+    }
+}
+
+class CastFormRequest extends FormRequest
+{
+    use CastAttribute;
+
+    protected $casts = [
+        'intAttribute' => 'int',
+        'floatAttribute' => 'float',
+        'stringAttribute' => 'string',
+        'boolAttribute' => 'bool',
+        'booleanAttribute' => 'boolean',
+        'arrayAttribute' => 'array',
+        'scalarAttribute' => 'array',
+        'jsonAttribute' => 'json',
+        'collectionAttribute' => 'collection',
+        'dateAttribute' => 'date',
+        'datetimeAttribute' => 'datetime',
+        'timestampAttribute' => 'timestamp',
+    ];
+
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    public function rules(): array
+    {
+        return [];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
